c1e6e73c34f7d47bdb4397189bf4880b607092ef
MichaelRoosz <michael.roosz@web.de>
2023-07-25 14:56:07 +0200

    Update ssh-tunnel-manager.rb

--- Formula/ssh-tunnel-manager.rb
+++ Formula/ssh-tunnel-manager.rb
@@ -1,8 +1,8 @@
 class SshTunnelManager < Formula
   desc "SSH tunnel management tool impkemented with xbar"
   homepage "https://github.com/MichaelRoosz/ssh-tunnel-manager"
-  url "https://github.com/MichaelRoosz/ssh-tunnel-manager/archive/refs/tags/v1.0.4.tar.gz"
-  sha256 "37b6f0757deaad89192ee0ba54e7f6c6aff86ac4100270076fda1876f7fe5577"
+  url "https://github.com/MichaelRoosz/ssh-tunnel-manager/archive/refs/tags/v1.0.5.tar.gz"
+  sha256 "778c352fe32bcf14d138813fb620ba2c7147754cfb41db63d29813cf0f5ef740"
   license "GPL-2.0-or-later"
 
   depends_on "jq"
